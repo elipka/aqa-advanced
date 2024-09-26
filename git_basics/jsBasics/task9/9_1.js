b76cf8137f14d069adc8f59c6e9012ba9b4f1ddd
elipka <eliazvettta.lipka@gmail.com>
2024-09-26 22:16:07 +0300

    task9

---
+++ git_basics/jsBasics/task9/9_1.js
@@ -0,0 +1,8 @@
+const student = {};
+
+student.firstName= "Lisa";
+student.lastName= "Lipka";
+student.age= 24;
+student.courses= ["math", "history", "art"];
+
+console.log(student)
---
+++ git_basics/jsBasics/task9/9_2.js
@@ -0,0 +1,10 @@
+const book = {
+    title: "somename",
+    author: "somewriter",
+    year: 2000
+};  
+
+const { title, author } = book;
+
+console.log(title);
+console.log(author);
